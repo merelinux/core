2258d7a8e1cf61329e9b4231199d7d6513ccfae8
Jeremy Huntwork <jeremy@merelinux.org>
2021-08-24 10:51:02 -0400

    Merge pull request #231 from jhuntwork/jh
    Re-organize scripts

---
+++ ci-scripts/ci-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+# shellcheck disable=SC2154,SC1090
+bn="$(git rev-parse --abbrev-ref HEAD)"
+if [ "$bn" != 'master' ] ; then
+    . "$CIRCLE_WORKING_DIRECTORY"/.env
+    if [ -n "$pkg" ] && [ "$is_deleted" = 'false' ]; then
+        sudo MEREDIR="/tmp/.mere" ./buildpkg.sh "$pkg"
+    else
+        printf 'No packages are required to build in this commit.\n'
+    fi
+fi
